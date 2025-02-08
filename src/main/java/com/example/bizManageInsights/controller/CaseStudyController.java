140c7f43fc3cab7347623f0021700d178a47d387
VIPERtips <mytipstadiwa.gmail.com>
2025-02-08 18:42:33 +0200

    cross origin url added

--- src/main/java/com/example/bizManageInsights/controller/CaseStudyController.java
+++ src/main/java/com/example/bizManageInsights/controller/CaseStudyController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/case-studies")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3000","https://biz-manage-insights.vercel.app"})
 public class CaseStudyController {
 
     @Autowired
